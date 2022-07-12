654693073d6ecf6209f77aadc64f767c45230c74
nelloolisarun <sarunalena@gmail.com>
2022-07-12 22:53:41 +0530

    initi

---
+++ lib/bumil.dart
@@ -0,0 +1,9 @@
+void main() {
+  buyMilk(7);
+}
+
+void buyMilk(int day) {
+  for (var i = 1; i < day; i++) {
+    print('but milk for day $i');
+  }
+}
---
+++ lib/coin_data.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = ['BTC', 'ETH', 'LTC'];
+
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate/';
+const apiKey = 'DCE739BE-C8D7-4B61-A964-D2A9CC3136C9';
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      String requestURL =
+          '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
+      http.Response response = await http.get(Uri.parse(requestURL));
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double price = decodedData['rate'];
+        cryptoPrices[crypto] = price.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        print(requestURL);
+        throw 'Problem with the get request';
+      }
+    }
+    return cryptoPrices;
+  }
+}
